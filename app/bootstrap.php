5dc39c7f6278014bea7ab293430606831a927ff2
toto <test@test.com>
2023-12-11 19:20:31 +0200

    autoloader

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,11 @@
 <?php
+
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', DIRECTORY_SEPARATOR);
+    $path = '..' . DIRECTORY_SEPARATOR . $class . '.php';
+    if (file_exists($path)) {
+        include_once $path;
+        return true;
+    }
+    return false;
+});
